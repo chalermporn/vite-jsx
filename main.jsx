ce2aa39433560e998403c2ea9702a69c675319a5
BIRD-MPC <bird@BIRD-MPCs-MacBook-Pro.local>
2020-05-11 00:40:56 +0700

    init

---
+++ main.jsx
@@ -0,0 +1,31 @@
+import { h, render, Component } from 'preact';
+
+class App extends Component {
+  // Add `name` to the initial state
+  state = { value: '', name: 'world' }
+
+  onInput = ev => {
+    this.setState({ value: ev.target.value });
+  }
+
+  // Add a submit handler that updates the `name` with the latest input value
+  onSubmit = ev => {
+    // Prevent default browser behavior (aka don't submit the form here)
+    ev.preventDefault();
+
+    this.setState({ name: this.state.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, {this.state.name}!</h1>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.value} onInput={this.onInput} />
+          <button type="submit">Update</button>
+        </form>
+      </div>
+    );
+  }
+}
+render(<App />, document.getElementById("app"));
